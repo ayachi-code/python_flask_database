31000502bb95bd87eb1cbfc8f23305a906ae130f
bilal <buddygames.bilal@gmail.com>
2017-12-27 19:03:36 +0100

    als je naar de index gaat(/) dan word home.html geshowt

--- code.py
+++ code.py
@@ -1,3 +1,12 @@
 #importeer FLask
 from flask import *
 app = Flask(__name__)
+
+
+#als iemand naar / gaat
+@app.route('/')
+def laat_home_zien():
+    return render_template("home.html")
+
+
+app.run()
